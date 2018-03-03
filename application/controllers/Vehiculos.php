a97115ba3e633b667b36071c86350dc4c27dc17f
Brisingr <mdavid.almiron@gmail.com>
2018-03-03 20:11:35 -0300

    Vehiculos controller

---
+++ application/controllers/Vehiculos.php
@@ -0,0 +1,128 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Vehiculos extends CI_Controller {
+
+	public function __construct()
+	{
+	        parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function new()
+	{
+
+	}
+
+	public function create()
+	{
+		
+	}
+
+	public function edit()
+	{
+		
+	}
+
+	public function update()
+	{
+		
+	}
+
+	public function destroy()
+	{
+		
+	}
+
+
+	/* ===================== MARCAS VEHICULOS =============================
+	=======================================================================
+	======================================================================= */
+
+	public function get_marcas()
+	{
+		
+	}
+
+	public function create_marca()
+	{
+		
+	}
+
+	public function edit_marca()
+	{
+		
+	}
+
+	public function update_marca()
+	{
+		
+	}
+
+	public function destroy_marca()
+	{
+		
+	}
+
+	/* ===================== MODELOS VEHICULOS ============================
+	=======================================================================
+	======================================================================= */
+	public function get_modelos()
+	{
+		
+	}
+
+	public function create_modelo()
+	{
+		
+	}
+
+	public function edit_modelo()
+	{
+		
+	}
+
+	public function update_modelo()
+	{
+		
+	}
+
+	public function destroy_modelo()
+	{
+		
+	}
+
+
+	/* ===================== TIPOS VEHICULOS ==============================
+	=======================================================================
+	======================================================================= */
+	public function get_tipos()
+	{
+		
+	}
+
+	public function create_tipo()
+	{
+		
+	}
+
+	public function edit_tipo()
+	{
+		
+	}
+
+	public function update_tipo()
+	{
+		
+	}
+
+	public function destroy_tipo()
+	{
+		
+	}
+
+}
